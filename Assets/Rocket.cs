8a786f240a70eeae55854703e5e49a8d10867e21
ardaboluk <ardaboluk2@gmail.com>
2019-07-28 19:22:12 +0300

    Added rotation controls to the ship.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+
+    private float rotation_const;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotation_const = 5;
+
+        rigidBody = GetComponent<Rigidbody>();
+        //rigidBody = GetComponent(typeof(Rigidbody)) as Rigidbody;
+        //rigidBody = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,27 +29,34 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.A))
         {
-            print("Left and Thrusting");
+            rigidBody.AddRelativeForce(new Vector3(0, 1, 0));
+            transform.Rotate(rotation_const * Time.deltaTime * Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.D))
         {
-            print("Right and Thrusting");
+            rigidBody.AddRelativeForce(new Vector3(0, 1, 0));
+            transform.Rotate((-1) * rotation_const * Time.deltaTime * Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
-            print("Left and Right");
+            ;
+        }
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(new Vector3(0, 1, 0));
         }
         else if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(new Vector3(0, 1, 0));
+            //rigidBody.AddRelativeForce(Vector3.up);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            print("Left");
+            transform.Rotate(5 * Time.deltaTime * Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Right");
+            transform.Rotate((-1) * 5 * Time.deltaTime * Vector3.forward);
         }        
     }
 }
