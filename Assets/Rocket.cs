ed7c645f4d723cbbf56796b273a7e3f15771f4b4
ardaboluk <ardaboluk2@gmail.com>
2019-08-01 15:40:14 +0300

    + Modified gitignore file to include .ogg and .ogg.meta + Modified the rigid body of the ship so that the mass is 1. + Created a new tag named Friendly and tagged Launchpad with it. Also applied it to its prefab. + Added obstacle and a corresponding material. Not prefabbed the obstacle.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -8,13 +8,15 @@ public class Rocket : MonoBehaviour
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-    private float rotation_const;
+    [SerializeField] private float rotation_const;
+    [SerializeField] private float thrust_const;
     private bool audioRocketThrustPlaying = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotation_const = 20;
+        rotation_const = 250;
+        thrust_const = 40;
 
         rigidBody = GetComponent<Rigidbody>();
         //rigidBody = GetComponent(typeof(Rigidbody)) as Rigidbody;
@@ -29,11 +31,24 @@ public class Rocket : MonoBehaviour
         ManageRotate();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void ManageThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(new Vector3(0, 1, 0));
+            rigidBody.AddRelativeForce(new Vector3(0, thrust_const, 0));
             if(audioSource.isPlaying == false)
             {
                 audioSource.Play();
